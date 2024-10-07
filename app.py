ba683c00fe086df8df961663a3fd1029c65d5396
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-10-07 10:36:35 -0300

    ta-da: initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from rich.traceback import install
+
+# Rich traceback support (better error messages)
+install()
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
